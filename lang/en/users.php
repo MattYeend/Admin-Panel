23a45981ebafb0974ac958bfcc6234349bc728ff
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-04 08:20:13 +0000

    #53 New users english file

---
+++ lang/en/users.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title' => 'Title',
+    'first_name' => 'First Name',
+    'middle_name' => 'Middle Name',
+    'last_name' => 'Last Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'role' => 'Role',
+    'create_user' => 'Create User',
+    'update_user' => 'Update User',
+    'delete_user' => 'Delete User',
+    'created_success' => 'User Created Successfully',
+    'updated_success' => 'User Updated Successfully',
+    'deleted_success' => 'User Deleted Successfully',
+    'actions' => 'Actions',
+    'confirm_delete' => 'Are you sure?',
+];
