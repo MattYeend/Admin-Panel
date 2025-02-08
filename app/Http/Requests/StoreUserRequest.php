2ea4dc5a65ee8c5769925a05f0015beb3c48166b
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-08 12:08:33 +0000

    #59 Added to storeusersrequest

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -30,6 +30,7 @@ class StoreUserRequest extends FormRequest
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'nullable|string|min:8|confirmed',
             'role' => 'required|in:admin,editor,user',
+            'phone_number' => 'required|string|max:15|regex:/^[0-9+\-\s]+$/',
         ];
     }
 }
