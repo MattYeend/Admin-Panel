051ea1b31b3795e9967c226c15384c026327e7dd
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-08 11:49:39 +0000

    #56 New en file for departments

---
+++ lang/en/departments.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'department_name' => 'Department Name',
+    'department_lead' => 'Department Lead',
+    'department_lead_email' => 'Department Lead Email Address',
+    'department_lead_phone' => 'Department Lead Phone Number',
+    'department_type' => 'Department Type',
+    'department_location' => 'Department Location',
+    'create_department' => 'Create Department',
+    'update_department' => 'Update Department',
+    'delete_department' => 'Delete Department',
+    'created_success' => 'Department Created Successfully',
+    'updated_success' => 'Department Updated Successfully',
+    'deleted_success' => 'Department Deleted Successfully',
+    'actions' => 'Actions',
+    'confirm_delete' => 'Are you sure?',
+];
