cd7dcd21ba14df93087e168bfd2a77619bc81a93
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-08 11:52:38 +0000

    #56 New es file for departments

---
+++ lang/es/departments.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'department_name' => 'Nombre del Departamento',
+    'department_lead' => 'Líder del Departamento',
+    'department_lead_email' => 'Correo Electrónico del Líder del Departamento',
+    'department_lead_phone' => 'Teléfono del Líder del Departamento',
+    'department_type' => 'Tipo de Departamento',
+    'department_location' => 'Ubicación del Departamento',
+    'create_department' => 'Crear Departamento',
+    'update_department' => 'Actualizar Departamento',
+    'delete_department' => 'Eliminar Departamento',
+    'created_success' => 'Departamento Creado Exitosamente',
+    'updated_success' => 'Departamento Actualizado Exitosamente',
+    'deleted_success' => 'Departamento Eliminado Exitosamente',
+    'actions' => 'Acciones',
+    'confirm_delete' => '¿Estás seguro?',
+];
